59bcf427d8a9e4c7d87ccf0205f0b350e57967fa
adun1 <bmccormack15@gmail.com>
2020-06-05 00:00:49 +0000

    fnished Testing refactored cats

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,26 +1,24 @@
 const request = require('request');
 
 //pass in 1 cat to queury the database as a command line arg
-const breedFetcher = function(catArgs) {
-  if ((!catArgs) || (catArgs.length !== 1)) return; //1 cat at a time
-  
-  request("https://api.thecatapi.com/v1/breeds/search?q=" + catArgs[0], function(error, response, body) {
+const fetchBreedDescription = function(breedName, callback) {
+  request("https://api.thecatapi.com/v1/breeds/search?q=" + breedName, function(error, response, body) {
+    let description = null;
+
     if (error) {
-      console.error('error:', error); // print the error if one occured
+      callback(error, description);
       return;
     }
 
-    // console.log('statusCode:', response && response.statusCode); //print response status
-    
     const cat = JSON.parse(body);
     if (!cat.length) {
-      console.log("Breed Not Found");
-      return;
+      description = "Breed Not Found";
+    } else {
+      description = cat[0].description;
     }
-    
-    console.log(cat[0].description);
-
+    callback(error, description);
   });
+  
 };
 
-breedFetcher(process.argv.slice(2));
+module.exports = fetchBreedDescription;
